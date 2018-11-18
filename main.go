63204a226df5592e5c95e255cc3f7f3aa3fa589a
Dimitri Roche <dimroc@gmail.com>
2018-11-18 16:44:26 -0500

    Add threshold parameter and 500 if difference > threshold

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
+	"fmt"
 	"log"
 	"math/big"
 	"net/http"
@@ -16,13 +18,13 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Panic("Must pass two Ethereum JSON-RPC Endpoints as arguments to compare block heights")
+	if len(os.Args) != 4 {
+		log.Panic("Must pass two Ethereum JSON-RPC Endpoints as arguments to compare block heights, and a threshold that defines a healthy height difference (e.g. 20).")
 	}
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: insecureSkipVerify()}
-	r, err := createRouter(os.Args[1], os.Args[2])
+	r, err := createRouter(os.Args[1], os.Args[2], os.Args[3])
 	checkError(err)
-	log.Print("Comparing block heights from ", os.Args[1], " and ", os.Args[2])
+	log.Print("Comparing block heights from ", os.Args[1], " and ", os.Args[2], ", erroring when difference is greater than ", os.Args[3])
 	checkError(r.Run())
 }
 
@@ -38,9 +40,9 @@ func insecureSkipVerify() bool {
 	return false
 }
 
-func createRouter(p1, p2 string) (*gin.Engine, error) {
+func createRouter(p1, p2, threshold string) (*gin.Engine, error) {
 	r := gin.Default()
-	hc, err := newHeightsController(p1, p2)
+	hc, err := newHeightsController(p1, p2, threshold)
 	if err != nil {
 		return nil, err
 	}
@@ -55,20 +57,23 @@ func checkError(err error) {
 }
 
 type heightsController struct {
-	client1 *client
-	client2 *client
+	threshold *big.Int
+	client1   *client
+	client2   *client
 }
 
-func newHeightsController(endpoint1, endpoint2 string) (*heightsController, error) {
+func newHeightsController(endpoint1, endpoint2, thresholdStr string) (*heightsController, error) {
+	threshold, errd := strconv.ParseUint(thresholdStr, 10, 32)
 	c1, err1 := rpc.Dial(normalizeLocalhost(endpoint1))
 	c2, err2 := rpc.Dial(normalizeLocalhost(endpoint2))
-	merr := multierr.Combine(err1, err2)
+	merr := multierr.Combine(errd, err1, err2)
 	if merr != nil {
 		return nil, merr
 	}
 	return &heightsController{
-		client1: &client{Client: c1, endpoint: endpoint1},
-		client2: &client{Client: c2, endpoint: endpoint2},
+		threshold: big.NewInt(int64(threshold)),
+		client1:   &client{Client: c1, endpoint: endpoint1},
+		client2:   &client{Client: c2, endpoint: endpoint2},
 	}, nil
 }
 
@@ -92,8 +97,9 @@ func (hc *heightsController) Index(c *gin.Context) {
 
 	difference := big.NewInt(0)
 	difference.Abs(difference.Sub(latest1.Number.ToInt(), latest2.Number.ToInt()))
-	c.JSON(200, gin.H{
+	resp := gin.H{
 		"difference": difference.String(),
+		"threshold":  fmt.Sprint(hc.threshold),
 		"endpoints": []interface{}{map[string]interface{}{
 			"url":    hc.client1.endpoint,
 			"number": latest1.Number,
@@ -101,7 +107,25 @@ func (hc *heightsController) Index(c *gin.Context) {
 			"url":    hc.client2.endpoint,
 			"number": latest2.Number,
 		}},
-	})
+	}
+	logJSON(resp)
+	c.JSON(statusCodeForDifference(hc.threshold, difference), resp)
+}
+
+func statusCodeForDifference(threshold, difference *big.Int) int {
+	if threshold.Cmp(difference) == -1 {
+		return 500
+	}
+	return 200
+}
+
+func logJSON(v gin.H) {
+	j, err := json.Marshal(v)
+	if err != nil {
+		log.Println("Error: unable to marshal event to JSON")
+		return
+	}
+	log.Println(string(j))
 }
 
 type client struct {
@@ -110,5 +134,5 @@ type client struct {
 }
 
 type block struct {
-	Number hexutil.Big
+	Number hexutil.Big // public for deserialization by rpc.Client
 }
